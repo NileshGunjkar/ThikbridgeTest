6bfa66db1c68dab0529933ca016e9663b9e85807
NileshGunjkar <74947848+NileshGunjkar@users.noreply.github.com>
2020-11-24 11:04:17 +0530

    Update RouteConfig.cs

--- ThikbridgeTest/App_Start/RouteConfig.cs
+++ ThikbridgeTest/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+or﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +16,7 @@ namespace ThikbridgeTest
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Inventory", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
